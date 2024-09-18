177ea08162546f6557832c54c3b1689ae6f6117d
rupesh-singh-33 <rupeshsinghmanjhi05@gmail.com>
2024-09-18 22:40:27 +0530

    Add new files

---
+++ operator.py
@@ -0,0 +1,17 @@
+# operator true or false
+print(2>1)
+
+#logical or and not
+print(6 > 5 or 3 > 5 )
+print(6 > 5 and 3 > 5)
+print(not 3 > 2)
+
+# conditional statement
+age = 19
+if age >= 18:
+    print("you are adult")
+    print("You acan vote")
+elif age < 18 and age > 3:
+    print("You are in school")  
+else:
+    print("Thank you")
